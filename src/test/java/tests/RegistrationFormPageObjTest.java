2eff3e8c2176f05b7ccb95717840c20c5aedfdc1
Alexandra Zabnenkova <novalserg@yandex.com>
2025-10-11 11:18:30 +0300

    another commit

--- src/test/java/tests/RegistrationFormPageObjTest.java
+++ src/test/java/tests/RegistrationFormPageObjTest.java
@@ -1,10 +1,14 @@
 package tests;
 
+import attach.Attach;
 import com.codeborne.selenide.Configuration;
 
 import org.junit.jupiter.api.*;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import pages.RegistrationPage;
 import pages.components.PracticeFormFillingResultComponent;
+
+import java.util.Map;
 
 import static com.codeborne.selenide.Selenide.*;
 
@@ -12,15 +16,31 @@ public class RegistrationFormPageObjTest {
 
     @BeforeAll
     static void setEnv() {
-        Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.pageLoadStrategy = "eager";
         Configuration.timeout = 10000;
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
     }
 
     @AfterAll
     static void closeWB() {
         closeWebDriver();
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
     }
 
     @Test
